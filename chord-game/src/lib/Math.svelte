77e8bcb3e54b7cb7e27c06db5f72eabfd46ac916
getrod <kevinlouisjean@gmail.com>
2022-11-03 18:14:39 -0400

    math module

---
+++ chord-game/src/lib/Math.svelte
@@ -0,0 +1,17 @@
+<script context="module">
+    /**
+     * Compares two sets for equality
+     * @param {Set<number>} s1 first set
+     * @param {Set<number>} s2 second set
+     */
+    export function setIsEqual(s1, s2) {
+        if (s1.size !== s2.size) return false;
+        let result = true;
+        s1.forEach((note) => {
+            if (!s2.has(note)) result = false;
+        })
+        return result;
+    }
+
+    
+</script>
---
+++ chord-game/src/routes/tests.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { onMount } from 'svelte';
+	import { setIsEqual } from '../lib/Math.svelte';
+
+    function test(condition, message) {
+        console.log(`${condition ? 'PASS' : 'FAIL'}: ${message}`)
+    }
+
+	function set_is_equal_test() {
+		test(setIsEqual(new Set([67, 72]), new Set([67, 72])) === true, "set equality");
+		test(setIsEqual(new Set([67, 72]), new Set([67])) === false, "set inequality");
+	}
+
+    onMount(() => {
+        set_is_equal_test()
+    })
+</script>
