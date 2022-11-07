0ad96866e71676b40ac92341de1dd4636a93420e
getrod <kevinlouisjean@gmail.com>
2022-11-07 11:07:03 -0500

    add fluid synth

--- midi_server.py
+++ midi_server.py
@@ -1,12 +1,25 @@
 from __future__ import print_function
 
 import socketio
+import fluidsynth
 import time
 import sys
 from rtmidi import (midiutil, MidiIn, midiconstants)
 
-
 sio = socketio.Client()
+fs = fluidsynth.Synth()
+
+def start_synth():
+    fs.start()
+    dir = 'sound_fonts'
+    sfid = fs.sfload(f'{dir}/Nice-Steinway-Lite-v3.0.sf2')
+    fs.program_select(0, sfid, 0, 0)
+
+def play_midi(midi):
+    if midi['midi_event'] == 'note_on':
+        fs.noteon(0, midi['note'], midi['velocity'])
+    else:
+        fs.noteoff(0, midi['note'])        
 
 def midi_callback(event, data=None):
     message, deltatime = event
@@ -22,12 +35,14 @@ def midi_callback(event, data=None):
         'velocity': message[2]
     }
 
+    play_midi(midi)
     sio.emit('midi_event', midi)
 
 
 def start():
     midiin = MidiIn()
-
+    
+    ''' socket callbacks '''
     sio.on('get_midi_ports')
     def on_get_midi_ports():
         print('get_midi_ports')
@@ -40,13 +55,18 @@ def start():
         midiin.set_callback(midi_callback) 
         sio.emit('is_port_open', midiin.is_port_open())
 
+    ''' open midi input '''
     try:
         midiin, port_name = midiutil.open_midiinput()
     except (EOFError, KeyboardInterrupt):
         sys.exit()
-
     midiin.set_callback(midi_callback) 
+
+    ''' connect socket '''
     sio.connect('http://localhost:3000')
+
+    ''' start synth '''
+    start_synth()
 
     print("Entering main loop. Press Control-C to exit.")
     try:
@@ -57,6 +77,8 @@ def start():
     finally:
         print("Exit.")
         midiin.close_port()
+        fs.delete()
         del midiin
+
 
 start()
