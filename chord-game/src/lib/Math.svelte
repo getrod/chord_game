9588d16bcd449fb1983346e26f4baae5810d671e
getrod <kevinlouisjean@gmail.com>
2022-11-07 15:31:35 -0500

    simplify notesMatch function

--- chord-game/src/lib/Math.svelte
+++ chord-game/src/lib/Math.svelte
@@ -4,7 +4,7 @@
      * @param {Set<number>} s1 first set
      * @param {Set<number>} s2 second set
      */
-    export function setIsEqual(s1, s2) {
+    export function isSetEqual(s1, s2) {
         if (s1.size !== s2.size) return false;
         let result = true;
         s1.forEach((note) => {
